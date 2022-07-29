36220a2b5dbb0be5a79c8c5dd92504c3a1708f58
Suresh Ramani <58903201+suresh-ramani@users.noreply.github.com>
2022-07-29 11:14:40 +0530

    Update Default.vue
    update url

--- resources/js/components/layouts/Default.vue
+++ resources/js/components/layouts/Default.vue
@@ -2,7 +2,7 @@
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="https://techvblogs.com/blog/spa-authentication-laravel-9-sancum-vue3-vite" target="_blank">TechvBlogs</a>
+                <a class="navbar-brand" href="https://techvblogs.com/blog/spa-authentication-laravel-9-sanctum-vue3-vite" target="_blank">TechvBlogs</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
